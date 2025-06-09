52678f5dd5fbcdecd123eb1dbf19454a4a1571eb
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-06-09 00:52:58 +0000

    d

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,5 @@
-import java.util.*;
-
 public class Main {
 
 	public static void main(String args[]) {
-		//Test values for shop sort functions
-		String[] names = {"Toby", "Radin", "Olivier", "Hernandez"};
-		int[] prices = {300, 250, 200, 225};
-		int[] damage = {30, 31, 30, 500};
-		int[] health = {42, 30, 31, 999};
-
-		Shop shop = new Shop(names, prices);
-
-		Shop shopStats = new Shop(damage, health);
-		//before sort
-		shop.displayInventory();
-		//Sort price
-		shop.sortPrice();
-		//Shows shop
-		shop.displayInventory();
 	}
 }
